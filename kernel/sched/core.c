c1e7c3337973ee4ee353341f497b4e8ae9e62ec1
artas182x <artas182x@gmail.com>
2014-02-27 19:43:28 +0100

    Fix sched

--- kernel/sched/core.c
+++ kernel/sched/core.c
@@ -2149,6 +2149,16 @@ unsigned long long nr_context_switches(void)
 	return sum;
 }
 
+unsigned long avg_nr_running(void)
+{
+	unsigned long i, sum = 0;
+
+	for_each_online_cpu(i)
+		sum += cpu_rq(i)->ave_nr_running;
+
+	return sum;
+}
+
 unsigned long nr_iowait(void)
 {
 	unsigned long i, sum = 0;
